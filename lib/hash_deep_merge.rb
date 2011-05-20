ec897884289089ace2b6ab390dc81525cfc0a083
Offirmo <offirmo.net@gmail.com>
2011-05-20 23:29:55 +0200

    working, with some unit tests

---
+++ lib/hash_deep_merge.rb
@@ -0,0 +1,44 @@
+class Hash
+	
+	def deep_merge!(specialized_hash)
+		internal_deep_merge!(self, specialized_hash)
+	end
+	
+	
+	def deep_merge(specialized_hash)
+		internal_deep_merge!(Hash.new.replace(self), specialized_hash)
+	end
+	
+	
+	protected
+		
+		# better, recursive, preserving method
+		def internal_deep_merge!(source_hash, specialized_hash)
+			
+			#puts "starting deep merge..."
+			
+			specialized_hash.each_pair do |rkey, rval|
+				#puts "   potential replacing entry : " + rkey.inspect
+				
+				if source_hash.has_key?(rkey) then
+					#puts "   found potentially conflicting entry for #{rkey.inspect} : #{rval.inspect}, will merge :"
+					if rval.is_a?(Hash) and source_hash[rkey].is_a?(Hash) then
+						#puts "      recursing..."
+						internal_deep_merge!(source_hash[rkey], rval)
+					elsif rval == source_hash[rkey] then
+						#puts "      same value, skipping."
+					else
+						#puts "      replacing."
+						source_hash[rkey] = rval
+					end
+				else
+					#puts "   found new entry #{rkey.inspect}, adding it..."
+					source_hash[rkey] = rval
+				end
+			end
+			
+			#puts "deep merge done."
+			
+			source_hash
+		end
+end
